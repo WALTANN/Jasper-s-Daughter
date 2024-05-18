9a854854227e9e1a98fd7781c3c4e48f4d9c63e3
Lev Saksonov <levasaksonov@yandex.ru>
2024-05-18 21:05:27 +0300

    апдейт спикера, добавление авторизации
    Добавление функции для аутентификации + основной функции с проверкой регистрации пользователя через телеграм бота и возможностью авторизации

--- AIspeaker.py
+++ AIspeaker.py
@@ -14,3 +14,28 @@ def load_users(filename):
             return json.load(file)
     else:
         return {}
+        
+# Функция для аутентификации пользователя в ГП
+def authenticate(users, login, password):
+    for user_id, user_info in users.items():
+        if user_info['login'] == login and user_info['password'] == password:
+            return user_id
+    return None
+
+def main():
+    users = load_users(filename)
+
+    # Запрос логина и пароля
+    login = input("Введите ваш логин: ")
+    password = input("Введите ваш пароль: ")
+
+    # Проверка данных
+    user_id = authenticate(users, login, password)
+    if user_id:
+        print("Авторизация успешна. Голосовой помощник активирован.")
+
+    else:
+        print("Неверный логин или пароль. Пожалуйста, зарегистрируйтесь через телеграм-бота.")
+
+if __name__ == "__main__":
+    main()
